8c36e5c65efb3aa347ded558eab2624d42ca223e
Javier García <javigcpgr@gmail.com>
2023-04-28 16:42:02 +0200

    function flipCard commit

--- main.js
+++ main.js
@@ -1,52 +1,109 @@
 const startButton = document.querySelector("#startBtn");
 const template = document.querySelector("#templateCard");
-const board = document.querySelector("#board");
-
-
-//startButton.addEventListener("click", startGame);//
-
 const main = document.querySelector("main");
-      const templateCard = document.querySelector("#templateCard");
+const cards = document.querySelectorAll(".card");
 
-      const data = [
-        {
-          frontImgSrc: "images/310795.jpg",
-          backImgSrc: "images/465254.jpg",
-        },
-        {
-          frontImgSrc: "images/474477.jpg",
-          backImgSrc: "images/465254.jpg",
-        },
-        {
-          frontImgSrc: "images/677323.jpg",
-          backImgSrc: "images/465254.jpg",
-        },
-        {
-            frontImgSrc: "images/677323.jpg",
-            backImgSrc: "images/465254.jpg",
-        },
-        {
-            frontImgSrc: "images/677323.jpg",
-            backImgSrc: "images/465254.jpg",
-        },
-        {
-            frontImgSrc: "images/677323.jpg",
-            backImgSrc: "images/465254.jpg",
-        },
-        {
-            frontImgSrc: "images/677323.jpg",
-            backImgSrc: "images/465254.jpg",
-        },
-        {
-            frontImgSrc: "images/677323.jpg",
-            backImgSrc: "images/465254.jpg",
-        },
-          
-      ];
+startButton.addEventListener("click", startGame);
 
-      data.forEach(({ frontImgSrc, backImgSrc }) => {
-        const card = templateCard.content.cloneNode(true).querySelector("#card");
-        card.querySelector(".cardFront img").src = frontImgSrc;
-        card.querySelector(".cardBack img").src = backImgSrc;
-        main.appendChild(card);
-      });
+
+
+//Images on Array//
+
+const data = [
+  {
+    backImgSrc: "images/310795.jpg",
+    frontImgSrc: "images/465254.jpg",
+  },
+  {
+    backImgSrc: "images/474477.jpg",
+    frontImgSrc: "images/465254.jpg",
+  },
+  {
+    backImgSrc: "images/677323.jpg",
+    frontImgSrc: "images/465254.jpg",
+  },
+  {
+    backImgSrc: "images/677323.jpg",
+    frontImgSrc: "images/465254.jpg",
+  },
+  {
+    backImgSrc: "images/677323.jpg",
+    frontImgSrc: "images/465254.jpg",
+  },
+  {
+    backImgSrc: "images/677323.jpg",
+    frontImgSrc: "images/465254.jpg",
+  },
+  {
+    backImgSrc: "images/677323.jpg",
+    frontImgSrc: "images/465254.jpg",
+  },
+  {
+    backImgSrc: "images/677323.jpg",
+    frontImgSrc: "images/465254.jpg",
+  },
+];
+
+const doubledData = [...data, ...data];
+
+//random card//
+
+function shuffle() {
+  const clonedArray = [...doubledData];
+  const randomArray = [];
+
+  for (let i = 0; i < 16; i++) {
+    const randomIndex = Math.floor(Math.random() * clonedArray.length);
+    const randomItem = clonedArray[randomIndex];
+
+    randomArray.push(randomItem);
+    clonedArray.splice(randomIndex, 1);
+  }
+
+  return randomArray;
+}
+
+// Start Game//
+
+function startGame() {
+  const cardsToRemove = document.querySelectorAll(".card");
+  cardsToRemove.forEach((element) => main.removeChild(element));
+
+  const shuffledCards = shuffle();
+
+
+
+  
+
+
+
+
+
+
+
+  shuffledCards.forEach(({ frontImgSrc, backImgSrc }) => {
+    const card = template.content.cloneNode(true).querySelector(".card");
+    card.querySelector(".cardFront img").src = frontImgSrc;
+    card.querySelector(".cardBack img").src = backImgSrc;
+    card.addEventListener("click", flipCard)
+    main.appendChild(card);
+  
+  });
+
+}  
+
+function flipCard(event) {
+  this.classList.add("flipped");
+  const flippedcard = document.querySelectorAll(".flipped:not(.matchCard)");
+  if (flippedcard.length === 2) {
+    compare(flippedcard);
+  }
+}
+
+function compare(comparedCard) {
+  let card1 = comparedCard[0];
+  
+  console.log(comparedCard);
+  //console.log(card1.firstElementChild.firstElementChild.src);
+  //
+}
---
+++ test.js
@@ -0,0 +1,36 @@
+//images//
+
+const cardsimg [{
+    frontImgSrc: "images/310795.jpg",
+    backImgSrc: "images/465254.jpg",
+  },
+  {
+    frontImgSrc: "images/474477.jpg",
+    backImgSrc: "images/465254.jpg",
+  },
+  {
+    frontImgSrc: "images/677323.jpg",
+    backImgSrc: "images/465254.jpg",
+  },
+  {
+      frontImgSrc: "images/677323.jpg",
+      backImgSrc: "images/465254.jpg",
+  },
+  {
+      frontImgSrc: "images/677323.jpg",
+      backImgSrc: "images/465254.jpg",
+  },
+  {
+      frontImgSrc: "images/677323.jpg",
+      backImgSrc: "images/465254.jpg",
+  },
+  {
+      frontImgSrc: "images/677323.jpg",
+      backImgSrc: "images/465254.jpg",
+  },
+  {
+      frontImgSrc: "images/677323.jpg",
+      backImgSrc: "images/465254.jpg",
+  },
+    
+];
